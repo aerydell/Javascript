b5da915211cf506bc0eebcb694c957e9dbdd5d59
aerydell <sklijnsma@roc-dev.com>
2018-12-10 14:37:47 +0100

    finished Part2

--- src/Part2/vulmetjs.js
+++ src/Part2/vulmetjs.js
@@ -7,3 +7,4 @@ document.getElementById("paginatitel").innerText = "My Awesome Page";
 document.getElementById("subkop").innerText = "My Awesome subkop";
 document.getElementById("aap1").src="img/aap1.jpg";
 document.getElementById("aap2").src="img/aap2.jpg";
+document.getElementById("wrapper").style.backgroundColor = "red";
