3d08e5b9f5d7706daee6ddc535948e24451fdb77
aerydell <sklijnsma@roc-dev.com>
2018-12-10 14:06:48 +0100

    finished part2

---
+++ src/Part2/vulmetjs.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+document.getElementById("paginatitel").innerText = "My Awesome Page";
+document.getElementById("subkop").innerText = "My Awesome subkop";
+document.getElementById("aap1").src="img/aap1.jpg";
+document.getElementById("aap2").src="img/aap2.jpg";
